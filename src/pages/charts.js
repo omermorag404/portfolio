1ade35659f6513916ec835322eb61e76858d22a8
omer morag <omm301@gmail.com>
2023-11-02 20:56:44 +0200

    charts edit

--- src/pages/charts.js
+++ src/pages/charts.js
@@ -1,19 +1,13 @@
 import React from "react";
-import { CartesianGrid, Line, LineChart, XAxis, YAxis } from "recharts";
-import { chartData } from "../components/data/dataDashboard";
-import colors from "../components/styles/colors";
+import { ChartLine } from "../components/dashboard/linechart";
+import { ChartPie } from "../components/dashboard/pieChart";
 import "../components/styles/style.css";
-const data = chartData;
 
 export const Charts = () => {
   return (
     <div>
-      <LineChart width={600} height={300} data={data} color={colors.text}>
-        <Line type="monotone" dataKey="uv" stroke={colors.text} />
-        <CartesianGrid stroke={colors.text} />
-        <XAxis dataKey="name" color={colors.text} />
-        <YAxis />
-      </LineChart>
+      <ChartPie />
+      <ChartLine />
     </div>
   );
 };
